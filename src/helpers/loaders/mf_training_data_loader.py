7dca8b67799de1a129c8647c850017915c600fa8
matt pauk <mapa7010@colorado.edu>
2025-05-18 22:01:15 -0600

    added support for fine tuning based on multiple choice method

--- src/helpers/loaders/mf_training_data_loader.py
+++ src/helpers/loaders/mf_training_data_loader.py
@@ -33,11 +33,33 @@ def get_tf_training_prompt(system_prompt, example_prompt, data, features, labels
             positive_prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=False)
         else:
             negative_prompts.append(tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=False))
-    random_index = random.randint(0, len(negative_prompts) - 1)
-    return [positive_prompt, negative_prompts[random_index]]
+    #random_index = random.randint(0, len(negative_prompts) - 1)
+    negative_prompts.append(positive_prompt)
+    return negative_prompts
+
+def get_mc_training_prompt(system_prompt, example_prompt, data, features, tokenizer, ground_truth):
+    dict = {}
+    for feature in features:
+        dict[feature] = data[feature]
+    formatted_example_prompt = example_prompt.format(**dict)
+    messages = [
+        {
+            "role": "system",
+            "content": system_prompt
+        },
+        {
+            "role": "user",
+            "content": formatted_example_prompt
+        },
+        {
+            "role": "assistant",
+            "content": constants.LABEL_TO_MC_LETTER[ground_truth]
+        }
+    ]
+    return [tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=False)]
 
 
-def get_training_data(data_input_path, rule_split_path, tokenizer):
+def get_training_data(data_input_path, rule_split_path, tokenizer, rules, rule_type='tf'):
     data = dataset_loader.load_moral_frame_data_parse_entity_labels(data_input_path)
     mf_labels = dataset_loader.load_frame_labels(data_input_path)
     role_labels = dataset_loader.load_role_labels(data_input_path)
@@ -56,58 +78,111 @@ def get_training_data(data_input_path, rule_split_path, tokenizer):
     ids_to_prompts = {}
 
     # rule one prompts
-    rule_one_data = data[['Id', 'Tweet', 'Label']].drop_duplicates()
-    for item, row in rule_one_data.iterrows():
-        prompts = get_tf_training_prompt(
-            constants.MF_TF_SYSTEM_PROMPT, 
-            constants.MORAL_FOUNDATION_PROMPT_EXAMPLE_FORMAT,
-            row,
-            ['Id', 'Tweet'], 
-            constants.MORAL_FOUNDATIONS,
-            tokenizer,
-            row['Label'])
-        ids_to_prompts[row['Id']] = prompts
-
+    if 'rule_one' in rules:
+        rule_one_data = data[['Id', 'Tweet', 'Label']].drop_duplicates()
+        for item, row in rule_one_data.iterrows():
+            if rule_type == 'tf':
+                prompts = get_tf_training_prompt(
+                    constants.MF_TF_SYSTEM_PROMPT, 
+                    constants.MORAL_FOUNDATION_PROMPT_EXAMPLE_FORMAT,
+                    row,
+                    ['Id', 'Tweet'], 
+                    constants.MORAL_FOUNDATIONS,
+                    tokenizer,
+                    row['Label'])
+            elif rule_type == 'mc':
+                prompts = get_mc_training_prompt(
+                    constants.MF_MC_SYSTEM_PROMPT,
+                    constants.MF_MC_EXAMPLE_FORMAT,
+                    row,
+                    ['Id', 'Tweet'],
+                    tokenizer,
+                    row['Label']
+                )
+            ids_to_prompts[row['Id']] = prompts
     # rule two prompts
-    rule_two_data = data[['Id', 'Tweet', 'Entity', 'EntityLabels']].drop_duplicates()
-    for item, row in rule_two_data.iterrows():
-        if pd.isna(row['Entity']):
-            continue
-        prompts = get_tf_training_prompt(
-            constants.MR_TF_SYSTEM_PROMPT, 
-            constants.MORAL_ROLE_PROMPT_EXAMPLE_FORMAT,
-            row,
-            ['Id', 'Tweet', 'Entity'], 
-            constants.MORAL_FOUNDATION_ROLE,
-            tokenizer, 
-            row['EntityLabels'])
-        ids_to_prompts[row['Id']].extend(prompts)
+    if 'rule_two' in rules:
+        rule_two_data = data[['Id', 'Tweet', 'Entity', 'EntityLabels']].drop_duplicates()
+        for item, row in rule_two_data.iterrows():
+            if pd.isna(row['Entity']):
+                continue
+            if rule_type == 'tf':
+                prompts = get_tf_training_prompt(
+                    constants.MR_TF_SYSTEM_PROMPT, 
+                    constants.MORAL_ROLE_PROMPT_EXAMPLE_FORMAT,
+                    row,
+                    ['Id', 'Tweet', 'Entity'], 
+                    constants.MORAL_FOUNDATION_ROLE,
+                    tokenizer, 
+                    row['EntityLabels'])
+            elif rule_type == 'mc':
+                prompts = get_mc_training_prompt(
+                    constants.MR_MC_SYSTEM_PROMPT,
+                    constants.MR_MC_EXAMPLE_FORMAT,
+                    row,
+                    ['Id', 'Tweet', 'Entity'], 
+                    tokenizer,
+                    row['EntityLabels']
+                )
+            if row['Id'] in ids_to_prompts:
+                ids_to_prompts[row['Id']].extend(prompts)
+            else:
+                ids_to_prompts[row['Id']] = prompts
+
     # rule three prompts
-    rule_three_data = data[['Id', 'Tweet', 'Topic', 'Ideology', 'Label']].drop_duplicates()
-    for item, row in rule_three_data.iterrows():
-        prompts = get_tf_training_prompt(
-            constants.MF_TF_SYSTEM_PROMPT, 
-            constants.MORAL_FOUNDATION_PROMPT_WITH_FEATURES_EXAMPLE_FORMAT,
-            row,
-            ['Id', 'Tweet', 'Topic', 'Ideology'],
-            constants.MORAL_FOUNDATIONS,
-            tokenizer, 
-            row['Label'])
-        ids_to_prompts[row['Id']].extend(prompts)
+    if 'rule_three' in rules:
+        rule_three_data = data[['Id', 'Tweet', 'Topic', 'Ideology', 'Label']].drop_duplicates()
+        for item, row in rule_three_data.iterrows():
+            if rule_type == 'tf':
+                prompts = get_tf_training_prompt(
+                    constants.MF_TF_SYSTEM_PROMPT, 
+                    constants.MORAL_FOUNDATION_PROMPT_WITH_FEATURES_EXAMPLE_FORMAT,
+                    row,
+                    ['Id', 'Tweet', 'Topic', 'Ideology'],
+                    constants.MORAL_FOUNDATIONS,
+                    tokenizer, 
+                    row['Label'])
+            elif rule_type == 'mc':
+                prompts = get_mc_training_prompt(
+                    constants.MF_MC_SYSTEM_PROMPT,
+                    constants.MF_MC_EXAMPLE_FORMAT_WITH_FEATURES,
+                    row,
+                    ['Id', 'Tweet', 'Topic', 'Ideology'],
+                    tokenizer,
+                    row['Label']
+                )
+            if row['Id'] in ids_to_prompts:
+                ids_to_prompts[row['Id']].extend(prompts)
+            else:
+                ids_to_prompts[row['Id']] = prompts
 
     # rule four prompts
-    rule_four_data = data[['Id', 'Tweet', 'Entity', 'Ideology', 'Topic', 'EntityLabels']].drop_duplicates()
-    for item, row in rule_four_data.iterrows():
-        if pd.isna(row['Entity']):
-            continue
-        prompts = get_tf_training_prompt(
-            constants.MR_TF_SYSTEM_PROMPT, 
-            constants.MORAL_ROLE_PROMPT_WITH_FEATURES_EXAMPLE_FORMAT,
-            row,
-            ['Id', 'Tweet', 'Entity', 'Ideology', 'Topic'], 
-            constants.MORAL_FOUNDATION_ROLE,
-            tokenizer, 
-            row['EntityLabels'])
-        ids_to_prompts[row['Id']].extend(prompts)
+    if 'rule_four' in rules:
+        rule_four_data = data[['Id', 'Tweet', 'Entity', 'Ideology', 'Topic', 'EntityLabels']].drop_duplicates()
+        for item, row in rule_four_data.iterrows():
+            if pd.isna(row['Entity']):
+                continue
+            if rule_type == 'tf':
+                prompts = get_tf_training_prompt(
+                    constants.MR_TF_SYSTEM_PROMPT, 
+                    constants.MORAL_ROLE_PROMPT_WITH_FEATURES_EXAMPLE_FORMAT,
+                    row,
+                    ['Id', 'Tweet', 'Entity', 'Ideology', 'Topic'], 
+                    constants.MORAL_FOUNDATION_ROLE,
+                    tokenizer, 
+                    row['EntityLabels'])
+            elif rule_type == 'mc':
+                prompts = get_mc_training_prompt(
+                    constants.MR_MC_SYSTEM_PROMPT,
+                    constants.MR_MC_EXAMPLE_FORMAT_WITH_FEATURES,
+                    row,
+                    ['Id', 'Tweet', 'Entity', 'Ideology', 'Topic'], 
+                    tokenizer,
+                    row['EntityLabels']
+                )
+            if row['Id'] in ids_to_prompts:
+                ids_to_prompts[row['Id']].extend(prompts)
+            else:
+                ids_to_prompts[row['Id']] = prompts
 
     return train_test_splits, ids_to_prompts
