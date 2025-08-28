91487e46660eacedc6babf3cea3fb8c0ef5ed851
matt pauk <mapa7010@colorado.edu>
2025-08-28 08:38:02 -0600

    prompt template setup

--- src/helpers/prompting/econ_prompt_constants.py
+++ src/helpers/prompting/econ_prompt_constants.py
@@ -51,3 +51,29 @@ QUANTITY_POLARITY_LABELS = [
     "negative",
     "neutral"
 ]
+
+TF_ARTICLE_TYPE_SYSTEM_PROMPT = '''
+Consider the task of identifying the type of economic information covered in a U.S News Article. Given the content of the article, answer the following true/false question corresponding to whether or not a specific type of economic information was most prevalent in the article.'''
+
+TF_ARTICLE_TYPE_EXAMPLE_PROMPT = '''
+Article Text: {Text}
+
+Q. The main type of economic information covered in the article is {label} - true or false? A.'''
+
+TF_ARTICLE_CONDITIONS_SYSTEM_PROMPT = '''
+Consider the task of identifying the general economic conditions based on the economic information covered in a U.S News Article. Given the content of the article, answer the following true/false question corresponding to the framing of the general economic conditions.'''
+
+TF_ARTICLE_CONDITIONS_USER_PROMPT = '''
+Article Text: {Text}
+
+Q. The main type of economic information covered in the article is {label} - true or false? A.'''
+
+TF_ARTICLE_DIRECTION_USER_PROMPT = '''
+Consider the task of identifying the type of economic information covered in a U.S News Article. Given the content of the article, answer the following true/false question corresponding to whether or not a specific type of economic information was most prevalent in the article.'''
+
+TF_ARTICLE_DIRECTION_USER_PROMPT = '''
+Article Text: {Text}
+
+Q. The main type of economic information covered in the article is {label} - true or false? A.'''
+
+
